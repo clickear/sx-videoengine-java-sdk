4443996f7e8454dcb5501795271b9a0488916805
dongyado <dongyado@gmail.com>
2019-05-16 11:19:22 +0800

    1.3.8 release

--- workspace/main.lua
+++ workspace/main.lua
@@ -1,15 +1,15 @@
 function main(context, data)
+    print("lua main ran")
 
-print("lua main func ran")
--- set watermark
-context:addWatermark("/home/slayer/Desktop/workspace/watermark.png", 100, 100, 0, 0, 0.5, 0.5)
-
--- add audio
---local audio1 = AudioManager.addTrack(context, "/home/slayer/Desktop/workspace/music.mp3")
-local audio2 = AudioManager.addTrack(context, "/home/slayer/Desktop/workspace/china_style.mp3")
---AudioManager.setOutPoint(context, audio2, 50)
+    context:addWatermark("/home/slayer/Desktop/workspace/watermark.png", 100, 100, 0, 0, 0.5, 0.5)
+    local audio1 = AudioManager.addTrack(context, "/home/slayer/Desktop/workspace/music.mp3")
+    local audio2 = AudioManager.addTrack(context, "/home/slayer/Desktop/workspace/result.aac")
 
 
-print("audio tracks size :", AudioManager.numTracks(context))
+    print("duration 1 :", AudioManager.duration(context, audio1))
+
+    if (audio2) then
+        print("duration 2 :", AudioManager.duration(context, audio2))
+    end
 
 end
