587273b81c625271f0f07d75d09585ae03fde25f
dongyado <dongyado@gmail.com>
2019-08-11 11:45:53 +0800

    完善高级替换实体类的注释和方法

--- src/main/java/com/seeshiontech/vesdk/Asset.java
+++ src/main/java/com/seeshiontech/vesdk/Asset.java
@@ -5,10 +5,23 @@ import com.alibaba.fastjson.annotation.JSONField;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 替换素材实体类
+ *
+ * */
+
 public class Asset {
 
+
+    /**
+     * 主替换素材
+     * */
     @JSONField(name="main_file")
     private String mainFile;
+
+    /**
+     * 高级替换素材
+     * */
     private List<ReplaceAsset> replaces = new ArrayList<>();
 
     public Asset() {
@@ -47,6 +60,14 @@ public class Asset {
        replaces.add(replaceAsset);
     }
 
+
+    /**
+     * 构建精确查找替换的高级文字素材
+     *
+     * @param content, String, 文字内容
+     * @param key, String, 精确查找的 key
+     * @return ReplaceAsset
+     * */
     public ReplaceAsset buildTextAsset(String content, String key) {
         ReplaceAsset asset = new ReplaceAsset(2);
         asset.setText(content);
@@ -56,6 +77,13 @@ public class Asset {
         return asset;
     }
 
+    /**
+     * 构建前缀查找替换的高级文字素材
+     *
+     * @param content, String, 文字内容
+     * @param key, String, 前缀查找 key 的前缀
+     * @return ReplaceAsset
+     * */
     public ReplaceAsset buildPrefixTextAsset(String content, String key) {
         ReplaceAsset asset = new ReplaceAsset(2);
         asset.setText(content);
@@ -65,6 +93,14 @@ public class Asset {
         return asset;
     }
 
+
+    /**
+     * 构建精确查找的高级图片素材
+     *
+     * @param filePath, String, 文件路径
+     * @param key, String 精确查找的 key
+     * @return ReplaceAsset
+     * */
     public ReplaceAsset buildFileAsset(String filePath, String key) {
         ReplaceAsset asset = new ReplaceAsset(1);
         asset.setFile(filePath);
@@ -74,6 +110,15 @@ public class Asset {
         return asset;
     }
 
+
+    /**
+     * 构建前缀查找替换的高级图片素材
+     *
+     *
+     * @param filePath, String, 文件路径
+     * @param key, String, 前缀查找 key 的前缀
+     * @return ReplaceAsset
+     * */
     public ReplaceAsset buildPrefixFileAsset(String filePath, String key) {
         ReplaceAsset asset = new ReplaceAsset(1);
         asset.setFile(filePath);
@@ -83,22 +128,61 @@ public class Asset {
         return asset;
     }
 
+    /**
+     * 添加一个前缀查找替换的高级文字素材
+     *
+     * @param text, String 文字内容
+     * @param key, String 指定 ui_key 的前缀
+     * */
     public void addPrefixTextReplaceAsset(String text, String key) {
         replaces.add(buildPrefixTextAsset(text, key));
     }
+
+    /**
+     * 添加一个前缀查找替换的高级文字素材(默认前缀为 dtext)
+     *
+     * @param text, String 文字内容
+     * */
     public void addPrefixTextReplaceAsset(String text) {
         replaces.add(buildPrefixTextAsset(text, ""));
     }
 
+    /**
+     * 添加一个精确查找替换的高级文字素材
+     *
+     * @param text, String 文字内容
+     * @param key, String 查找的 key
+     * */
     public void addTextReplaceAsset(String text, String key) {
         replaces.add(buildTextAsset(text, key));
     }
 
+    /**
+     * 添加一个前缀查找替换的高级图片素材(默认前缀 dtext)
+     *
+     * @param filePath, String 文件路径
+     * */
     public void addPrefixFileReplaceAsset(String filePath) {
         replaces.add(buildPrefixFileAsset(filePath, ""));
     }
 
+    /**
+     * 添加一个前缀查找替换的高级图片素材
+     *
+     * @param filePath, String 文件路径
+     * @param key, String 查找的 key 前缀
+     * */
     public void addPrefixFileReplaceAsset(String filePath, String key) {
         replaces.add(buildPrefixFileAsset(filePath, key));
+    }
+
+    /**
+     * 添加一个精确查找替换的高级图片素材
+     *
+     * @param filePath, String 文件路径
+     * @param key, String 查找的 key
+     * */
+    public void addFileReplaceAsset(String filePath, String key) {
+        replaces.add(buildFileAsset(filePath, key));
     }
 }
--- src/main/java/com/seeshiontech/vesdk/ReplaceAsset.java
+++ src/main/java/com/seeshiontech/vesdk/ReplaceAsset.java
@@ -3,12 +3,81 @@ package com.seeshiontech.vesdk;
 
 import com.alibaba.fastjson.annotation.JSONField;
 
+/**
+ * 高级替换素材
+ *
+ * 高级文字替换素材示例:
+ *
+ * 前缀查找文字替换
+ * {
+ *     text: "你好",
+ *     type: 1,
+ *     d_key_prefix: "dtext"
+ * }
+ *
+ * 默认前缀(dtext)查找文字替换
+ * {
+ *     text: "你好",
+ *     type: 1
+ * }
+ *
+ * 精确查找文字替换
+ * {
+ *     text: "你好",
+ *     type: 1,
+ *     d_key: "title"
+ * }
+ *
+ * 高级图片替换素材示例:
+ *
+ * 前缀查找图片替换
+ * {
+ *     file: "/path/to/file.png",
+ *     type: 2,
+ *     d_key_prefix: "dtext"
+ * }
+ *
+ * 默认前缀(dtext)查找替换
+ * {
+ *     file: "/path/to/file.png",
+ *     type: 2
+ * }
+ *
+ * 精确查找替换
+ * {
+ *     file: "/path/to/file.png",
+ *     type: 2,
+ *     d_key: "title"
+ * }
+ *
+ *
+ * */
 public class ReplaceAsset {
+    /**
+    * 文字
+    * */
     private String text;
+
+    /**
+     * 类型, 1 - 图片， 2 - 文字
+     * */
     private int type;
+
+    /**
+     * 图片路径
+     * */
     private String file;
+
+
+    /**
+     * 精确替换查找 key
+     * */
     @JSONField(name="d_key")
     private String dKey;
+
+    /**
+     * 前缀查找替换前缀
+     * */
     @JSONField(name="d_key_prefix")
     private String dKeyPrefix;
 
